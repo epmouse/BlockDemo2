acf8d08001114f52cda361ec0e7db2381b1b1d7b
epmouse <18622117113@126.com>
2017-11-01 18:02:45 +0800

    添加区块同步注释 两台server测试成功

--- src/main/java/org/cityu/cs/ian/service/Threads/ISyncBlockService.java
+++ src/main/java/org/cityu/cs/ian/service/Threads/ISyncBlockService.java
@@ -6,6 +6,8 @@ import java.io.File;
 public interface ISyncBlockService {
     /**
      * 初始化的时候调用，发现有未同步的区块则下载同步
+     * 下载逻辑：如果下载完成总服务上又有了新的区块，则继续下载
+        知道与总服务器彻底同步才开始计算，防止下载的过程中，总服务器又生成了新的区块，导致不同步
      */
     void downloadBlock();
     void getBlockFileByName(String blockName);
